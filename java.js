adee70df70576a317a1c823ec9d786e05e577a52
y <X>
2025-02-17 13:17:19 +0100

    java valtozas

--- java.js
+++ java.js
@@ -3,8 +3,11 @@ window.addEventListener('DOMContentLoaded', fetchcharacters);
 async function fetchcharacters() {
     try {
         const response = await fetch('https://raw.githubusercontent.com/Laboratoria/LIM011-data-lovers/master/src/data/potter/potter.json');
-        console.log(response);
-    } catch (error) {
+        //console.log(response);
+        const characters = response.json(); 
+
         
+    } catch (error) {
+        console.error("Hiba történt a lekérdezésekor", error);
     }
 }
